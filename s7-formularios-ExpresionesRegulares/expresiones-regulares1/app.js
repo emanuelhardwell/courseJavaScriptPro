ccc9e936e57f231960fc3f84800b897896680db8
emanuelhardwell <emanuelnoe16@gmail.com>
2020-08-23 17:40:54 -0500

    Modificacion de JS de Expresiones Regulares 1

--- s7-formularios-ExpresionesRegulares/expresiones-regulares1/app.js
+++ s7-formularios-ExpresionesRegulares/expresiones-regulares1/app.js
@@ -1,10 +1,13 @@
 /* 
-link para construir EXPRESIONES REGULARES
-        https://regex101.com/
-*/
-
-/* G = buscar la palabra completa  
-   I= la palabra buscada puede ser MAYUSCULA O MINUSCULA O MIXTA
+Expresiones regulares:
+        Son una secuencia de caracteres que forma un patrón de búsqueda, principalmente utilizada para la búsqueda de patrones de cadenas de caracteres u operaciones de sustituciones.
+        https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Regular_Expressions
+    Página para evaluar expresiones regulares: https://regex101.com/
+    sintaxis:
+        /patron/
+    Banderas:
+        i: ignore case. No diferencia entre mayusculas y minúsculas
+        g: global. Busca de forma global, es decir, no se para después de la primera coincidencia
 */
 const parrafo = document.getElementById("parrafo").textContent;
 const regex = /lorem/g;
