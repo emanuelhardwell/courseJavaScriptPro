993eb969ae0558b109123306f05fa236b5cd6b9b
emanuelhardwell <emanuelnoe16@gmail.com>
2020-08-24 01:00:18 -0500

    SesionStorage y Local Storage

---
+++ s8-APIS-html/local-storage/app.js
@@ -0,0 +1,24 @@
+/*
+    API webStorage:
+        Los dos mecanismos en el almacenamiento web son los siguientes:
+            sessionStorage mantiene un área de almacenamiento separada para cada origen que está disponible mientras dure la sesión de la página (mientras el navegador esté abierto, incluyendo recargas de página y restablecimientos).
+            localStorage hace lo mismo, pero persiste incluso cuando el navegador se cierre y se reabra.
+        Ambos funcionan con clave:valor y tienen dos métodos fundamentales setItem() para asignar una clave:valor y getItem() que recibe como parámetro la clave de la que quieremos obtener el valor
+*/
+
+const formulario = document.getElementById("formulario");
+const select = document.getElementById("select");
+const parrafo = document.getElementById("parrafo");
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  localStorage.setItem(formulario.clave.value, formulario.valor.value);
+  select.innerHTML += `<option>${formulario.clave.value}</option>`;
+
+  formulario.reset();
+});
+
+select.addEventListener("change", () => {
+  const valor = localStorage.getItem(select[select.selectedIndex].textContent);
+  parrafo.textContent = valor;
+});
